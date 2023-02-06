31d5d56c79cfbdfa2f0b9de89148ee761f42178d
Victor <victor5055@outlook.com>
2023-02-06 18:12:20 -0500

    Add apollo sever and apply it to express server.

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// setup apollo server and use typeDefs, resolvers, and auth for context
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware
+  });
+  
+  // integrate apollo server with express app as middleware
+  server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +26,16 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+})
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+  console.log(`🌍 Now listening on localhost:${PORT}`);
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
+
+
