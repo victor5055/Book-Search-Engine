2320ddb5eab1175fbadd728acd7d457d8660c5ba
Victor <victor5055@outlook.com>
2023-02-08 17:48:45 -0500

    Change to connection.js.

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/book_search_engine', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Book-Search-Engine', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
