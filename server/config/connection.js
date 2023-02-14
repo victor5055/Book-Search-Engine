975a1e61bc1a6287c94c311d0f5e7cb59c49cf67
Victor <victor5055@outlook.com>
2023-02-14 12:46:30 -0500

    Database.

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require("dotenv").config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Book-Search-Engine', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/SearchForBook', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 
